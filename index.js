9f90fe7c3fafd7a027477fb68c7e5e679edba1be
Luke <lshillman@gmail.com>
2022-07-15 21:18:53 -0700

    add questions

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project title:'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Brief description of project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Your github username:'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Your email address:'
+    },
+];
+
+function beginInquiry(){
+    inquirer
+        .prompt(questions)
+        .then(writeToFile(data));
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile(`generated/README.md`, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    beginInquiry();
+}
 
 // Function call to initialize app
 init();
